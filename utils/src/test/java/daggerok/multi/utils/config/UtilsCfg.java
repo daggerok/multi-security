966e9c6ee3bb1558aadd5ce1377945d6b82ba653
Maksim Kostromin <daggerok@gmail.com>
2016-01-08 08:12:58 +0200

    fix

---
+++ utils/src/test/java/daggerok/multi/utils/config/UtilsCfg.java
@@ -0,0 +1,8 @@
+package daggerok.multi.utils.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "daggerok.multi.utils")
+public class UtilsCfg {}
